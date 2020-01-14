751f1bab62c55169cdaf876b6d9c98e1fee0cda1
Michael <Michael.rydl@gmail.com>
2020-01-15 00:06:14 +0100

    Done

--- app/app.js
+++ app/app.js
@@ -8,32 +8,38 @@ const apiKey = "8a0a03c6952264f306d6cb1cd3a812fa";
 
 form.addEventListener( "submit", e => {
   e.preventDefault();
-  weatherHeader.classList.add( "weather-header-move" );
-  weatherMain.classList.add( "weather-main-move" );
   const selectCity = select.value;
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${selectCity}&units=metric&appid=${apiKey}`;
 
-  fetch( url )
-    .then( response => response.json() )
-    .then( data => {
-      const { list } = data;
+  if ( selectCity === "choose-city" ) {
+    alert( "Musíte vybrat město." );
+    return;
+  } else {
+    weatherHeader.classList.add( "weather-header-move" );
+    weatherMain.classList.add( "weather-main-move" );
 
-      cityList.innerHTML = "";
+    fetch( url )
+      .then( response => response.json() )
+      .then( data => {
+        const { list } = data;
 
-      for ( i = 0; i < list.length; i++ ) {
-        const li = document.createElement( "li" );
-        li.classList.add( "city" );
-        const markup = `<div class="markup-section">
+        cityList.innerHTML = "";
+
+        for ( i = 0; i < list.length; i++ ) {
+          const li = document.createElement( "li" );
+          li.classList.add( "city" );
+          const markup = `<div class="markup-section">
                           <div class="city-main">${list[i].weather[0].main}</div>
                           <img class="city-icon" src="https://openweathermap.org/img/wn/${list[i].weather[0].icon}@2x.png"></img>
                           <div class="city-temp">${Math.round( list[i].main.temp )}<sup>°C</sup></div>
                           <br/>
                           <div class="city-date">${list[i].dt_txt}</div>
                         </div>`;
-        li.innerHTML = markup;
-        cityList.appendChild( li );
+          li.innerHTML = markup;
+          cityList.appendChild( li );
 
-        weatherCitySection.classList.add( "show-cities" );
-      }
-    } );
+          weatherCitySection.classList.add( "show-cities" );
+        }
+      } );
+  }
 } );
